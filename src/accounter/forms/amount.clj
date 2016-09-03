eef26c184811e5b91c1f111970835d4fdd37505c
Andrew Lee <smiley325squared@gmail.com>
2016-09-03 15:16:42 -0500

    Remove Clojure stuff.

--- src/accounter/forms/amount.clj
+++
@@ -1,8 +0,0 @@
-(ns accounter.forms.amount)
-
-(defn as-cents
-  "Recognize string as an amount and convert to a cents figure."
-  [s]
-  (->> (re-seq #"\d+" s)
-       (clojure.string/join)
-       (clojure.edn/read-string)))
